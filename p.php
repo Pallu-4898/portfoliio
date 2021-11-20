22251e8f8459723acfa312de6336d87b2717ef4b
Pallu-4898 <94760725+Pallu-4898@users.noreply.github.com>
2021-11-21 00:47:06 +0530

    Add files via upload

---
+++ p.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content=
+		"width=device-width, initial-scale=1.0">
+	<title>Portfolio</title>
+</head>
+
+<body bgcolor="black">
+	<!--Header(start)-->
+	<table id="header" border="0" width="100%"
+		cellpadding="0" cellspacing="0" bgcolor="#f49flc">
+		<tr>
+			<td>
+				<table border="0" cellpadding="15"
+					cellspacing="0" width="90%" align="center">
+					<tr>
+
+						<td>
+							<a href="#home" style="text-decoration:none">
+								<font face="Verdana" size="5" color=#000080>
+									Home
+								</font>
+							</a>
+						</td>
+
+						<td>
+							<a href="#about" style="text-decoration:none">
+								<font face="Verdana" size="5" color=#000080>
+									About
+								</font>
+							</a>
+						</td>
+
+						<td>
+							<a href="#projects" style="text-decoration:none">
+								<font face="Verdana" size="5" color=#000080>
+									Projects
+								</font>
+							</a>
+						</td>
+
+						<td>
+							<a href="#achievements" style="text-decoration:none">
+								<font face="Verdana" size="5" color=#000080>
+									Achievements
+								</font>
+							</a>
+						</td>
+
+						<td>
+							<a href="#contact" style="text-decoration:none">
+								<font face="Verdana" size="5" color=#000080>
+									Contact
+								</font>
+							</a>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+<!-- About me starts here -->
+	<table id="about" border="0" width="100%"
+		cellpadding="0" cellspacing="0" bgcolor="#000080">
+		<tr>
+			<td>
+				<table border="0" cellpadding="15"
+					cellspacing="0" width="80%" align="center">
+					<tr>
+						<td height="180" align="center"
+							valign="middle" colspan="2">
+							<font face="Verdana" size="7"
+								color="#f49flc">
+								About Me
+							</font>
+							<hr color="#f49flc" width="90">
+						</td>
+					</tr>
+
+					<tr>
+						<td width="40%">
+							<img src="C:\Users\OMEN\Downloads\portfolio\pic.jpg" height="400" width="400">        <!--image-->
+						</td>
+
+						<td width="60%">
+							<font face="Verdana" size="4"
+								color="white">
+								Thanks for showing your interest, here
+								is a quick story of me...
+								<hr color="#f49flc">
+								Hi I am Pallabesh Maharana<br>Currently i am pursing my btech in Computer Science and Engineering at GIET University, Gunupur
+							</font>
+						</td>
+					</tr>
+				</table>
+				<br><br><br><br>
+			</td>
+		</tr>
+	</table>
+	<!--About(end)-->
+
+
+	<!--Projects(start)-->
+	<table id="projects" border="0" width="100%"
+		cellpadding="0" cellspacing="0" bgcolor="#f49flc">
+		<tr>
+			<td>
+				<table border="0" cellpadding="15"
+					cellspacing="0" width="80%" align="center">
+					<tr>
+						<td height="180" align="center"
+							valign="middle" colspan="2">
+							<font face="Verdana" size="7"
+								color="#000080">
+								Projects
+							</font>
+							<hr color="#000080" width="90">
+						</td>
+					</tr>
+
+					<tr>
+						<td height="10">
+							<font face="Times New Roman"
+								size="5" color="black">
+								<ul>
+									<li>
+										<a href="#" style="text-decoration:none; color: black;" title="Registration page">
+										Registration Page ➲
+										</a>
+									</li> 
+
+									<li>
+										<hr color="#000080">
+										<a href="#" style="text-decoration:none; color: black;" title="Login page">
+										Login Page ➲
+										</a>
+									</li> 	
+								</ul>
+								<br><br><br><br><br>
+							</font>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+	<!--Projects(end)-->
+
+	<!--Achievement(start)-->
+	<table id="achievements" border="0" width="100%"
+		cellpadding="0" cellspacing="0" bgcolor="000080">
+		<tr>
+			<td>
+				<table border="0" cellpadding="15"
+					cellspacing="0" width="80%" align="center">
+					<tr>
+						<td height="180" align="center" valign="middle">
+							<font face="Verdana" size="7" color="#f49flc">
+								Achievements
+								<hr color="#f49flc" width="100">
+							</font>
+						</td>
+					</tr>
+
+					<tr>
+						<td>
+							<font face="Verdana" size="4" color="white">
+								<ul>
+									<li>
+										Completed AI and Machine Learning in <a href="ck12.me/pallabesh" style="color: white;" title="certificates">CAMPK12</a>
+									</li>
+										<br><br>
+									<li>
+										Introduction to Data Science in COGNITIVE CLASS, IBM
+									</li>
+								</ul>
+
+							</font>
+						</td>
+					</tr>
+				</table><br><br>
+			</td>
+		</tr>
+	</table>
+	<!--Achievement(end)-->
+
+
+	<!--Contact(start)-->
+	<table id="contact" border="0" width="100%"
+		cellpadding="0" cellspacing="0" bgcolor="#f49flc">
+		<tr>
+			<td>
+				<table border="0" cellpadding="15"
+					cellspacing="0" width="80%" align="center">
+					<tr>
+						<td height="180" align="center"
+							valign="middle" colspan="2">
+							<font face="Verdana" size="7"
+								color="#000080">
+								Contact
+							</font>
+							<hr color="#000080" width="90">
+						</td>
+					</tr>
+
+					<tr>
+						<td align="center" valign="top">
+							<table border="0" width="50%" cellpadding="15"
+								cellspacing="0" align="center" bgcolor="#000080">
+								<tr>
+									<td width="30%">
+										<hr color="#000080">
+										<font face="Verdana" size="4"
+											color="white">
+											Name : Pallabesh Maharana<br><br>
+											Email: m.pallabesh2@gmail.com<br><br>
+										</font>
+									</td>
+								</tr>
+							</table>
+						</td>
+
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+	<!--Contact(end)-->
+</body>
+
+</html>
